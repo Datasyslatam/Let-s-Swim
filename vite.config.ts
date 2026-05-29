194b1d4294695c3ffbe7c5095590b419becaff8a
Jorge-240 <andres.herreraote@gmail.com>
2026-05-29 18:59:28 -0500

    Ajustes en el slider y optimisación de los videos

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart({
+      server: {
+        preset: "node-server",
+      },
+    }),
+    react(),
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": `${process.cwd()}/src`,
+    },
+    dedupe: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "@tanstack/react-query",
+      "@tanstack/query-core",
+    ],
+  },
+});
